a484af615040340aeb4d8ba23472c7ca0e5a2ba1
Ömer Eldehil <155307194+OmerEldehil@users.noreply.github.com>
2024-03-15 19:54:14 +0300

    Updated In 15.03.2024

--- main.js
+++ main.js
@@ -16,6 +16,25 @@ window.onload = numbersOnCities;
 window.addEventListener('resize', numbersOnCities);
 
 
+let searchInput = document.querySelector(".search-div input");
+let searchBtn = document.querySelector(".search-div .searchBtn");
+
+searchBtn.onclick = function() {
+  let numbersOnCities = document.getElementsByClassName("studentsCount") || [];
+
+  Array.from(numbersOnCities).forEach((number) => {
+    console.log(number);
+    array = JSON.parse(window.localStorage.getItem(number.id));
+    array.forEach((student) => {
+      if(student.name === capitalizeFirstLetters(searchInput.value)) {
+        number.click();
+      }
+    })
+
+  });
+}
+
+
 
 const info = document.querySelector('.il-isimleri');
 
@@ -563,6 +582,15 @@ function updateList(cityId) {
 
   if (array.length > 0) {
     array.forEach(person => {
+      if(typeof person !== typeof {}) {
+        array = array.filter((student) => typeof student === typeof {});
+        window.localStorage.setItem(cityId, JSON.stringify(array));
+      }
+    })
+
+    
+
+    array.forEach(person => {
       let personElement = document.createElement("div");
       personElement.classList.add("person");
 
@@ -583,45 +611,93 @@ function updateList(cityId) {
       
       let deleteBtn = document.createElement("button");
       deleteBtn.classList.add("bin-button");
-      
-      deleteBtn.innerHTML = `
-        <svg
-          class="bin-top"
-          viewBox="0 0 39 7"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
-          <line
-            x1="12"
-            y1="1.5"
-            x2="26.0357"
-            y2="1.5"
-            stroke="white"
-            stroke-width="3"
-          ></line>
-        </svg>
-        <svg
-          class="bin-bottom"
-          viewBox="0 0 33 39"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <mask id="path-1-inside-1_8_19" fill="white">
-            <path
-              d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
-            ></path>
-          </mask>
-          <path
-            d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-            fill="white"
-            mask="url(#path-1-inside-1_8_19)"
-          ></path>
-          <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
-          <path d="M21 6V29" stroke="white" stroke-width="4"></path>
-        </svg>
-      `;
 
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        deleteBtn.innerHTML = "Sil";
+      }
+
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        deleteBtn.innerHTML = `
+          <svg
+            class="bin-top"
+            viewBox="0 0 39 7"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
+            <line
+              x1="12"
+              y1="1.5"
+              x2="26.0357"
+              y2="1.5"
+              stroke="white"
+              stroke-width="3"
+            ></line>
+          </svg>
+          <svg
+            class="bin-bottom"
+            viewBox="0 0 33 39"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask id="path-1-inside-1_8_19" fill="white">
+              <path
+                d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+              ></path>
+            </mask>
+            <path
+              d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+              fill="white"
+              mask="url(#path-1-inside-1_8_19)"
+            ></path>
+            <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
+            <path d="M21 6V29" stroke="white" stroke-width="4"></path>
+          </svg>
+        `;
+
+        deleteBtn.onmouseover = function() {
+          this.innerHTML = "Sil";
+        }
+  
+        deleteBtn.onmouseout = function() {
+          this.innerHTML =`<svg
+            class="bin-top"
+            viewBox="0 0 39 7"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
+            <line
+              x1="12"
+              y1="1.5"
+              x2="26.0357"
+              y2="1.5"
+              stroke="white"
+              stroke-width="3"
+            ></line>
+          </svg>
+          <svg
+            class="bin-bottom"
+            viewBox="0 0 33 39"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask id="path-1-inside-1_8_19" fill="white">
+              <path
+                d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
+              ></path>
+            </mask>
+            <path
+              d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
+              fill="white"
+              mask="url(#path-1-inside-1_8_19)"
+            ></path>
+            <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
+            <path d="M21 6V29" stroke="white" stroke-width="4"></path>
+          </svg>
+        `;
+        }  
+      }
 
       deleteBtn.onclick = async function() {
         console.log(cityId)
@@ -652,100 +728,73 @@ function updateList(cityId) {
         } 
       };
 
-      deleteBtn.onmouseover = function() {
-        this.innerHTML = "Sil";
-      }
-
-      deleteBtn.onmouseout = function() {
-        this.innerHTML =`<svg
-          class="bin-top"
-          viewBox="0 0 39 7"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <line y1="5" x2="39" y2="5" stroke="white" stroke-width="4"></line>
-          <line
-            x1="12"
-            y1="1.5"
-            x2="26.0357"
-            y2="1.5"
-            stroke="white"
-            stroke-width="3"
-          ></line>
-        </svg>
-        <svg
-          class="bin-bottom"
-          viewBox="0 0 33 39"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <mask id="path-1-inside-1_8_19" fill="white">
-            <path
-              d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
-            ></path>
-          </mask>
-          <path
-            d="M0 0H33H0ZM37 35C37 39.4183 33.4183 43 29 43H4C-0.418278 43 -4 39.4183 -4 35H4H29H37ZM4 43C-0.418278 43 -4 39.4183 -4 35V0H4V35V43ZM37 0V35C37 39.4183 33.4183 43 29 43V35V0H37Z"
-            fill="white"
-            mask="url(#path-1-inside-1_8_19)"
-          ></path>
-          <path d="M12 6L12 29" stroke="white" stroke-width="4"></path>
-          <path d="M21 6V29" stroke="white" stroke-width="4"></path>
-        </svg>
-      `;
-      }
-
+      
       btnsAbso.appendChild(deleteBtn);
 
 
       let editBtn = document.createElement("button");
       editBtn.className = "editBtn";
-      editBtn.innerHTML = `
-      <svg height="1em" viewBox="0 0 512 512">
-        <path
-          d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-        ></path>
-      </svg>
-      `
 
-      editBtn.onclick = function() {
-        addStudentPrompt(cityId, person.name, person.university, person.major, person.hisClass, person.phone, person.homeland, true);
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        editBtn.innerHTML = "Bilgileri\nDüzenle";
       }
 
-      editBtn.onmouseover = function() {
-        this.innerHTML = "Bilgileri\nDüzenle";
-      }
-
-      editBtn.onmouseout = function() {
-        this.innerHTML = `
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        editBtn.innerHTML = `
         <svg height="1em" viewBox="0 0 512 512">
           <path
             d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
           ></path>
         </svg>
         `;
+
+        editBtn.onmouseover = function() {
+          this.innerHTML = "Bilgileri\nDüzenle";
+        }
+  
+        editBtn.onmouseout = function() {
+          this.innerHTML = `
+          <svg height="1em" viewBox="0 0 512 512">
+            <path
+              d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+            ></path>
+          </svg>
+          `;
+        }
       }
+
+      editBtn.onclick = function() {
+        addStudentPrompt(cityId, person.name, person.university, person.major, person.hisClass, person.phone, person.homeland, true);
+      }
+
+      
 
       btnsAbso.appendChild(editBtn)
 
 
       let noteBtn = document.createElement("button");
       noteBtn.className = "noteBtn";
-      noteBtn.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg>`;
-      
+
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        noteBtn.innerHTML = "Not<br>Ekle/<br>Sil";
+      }
+
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        noteBtn.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg>`;
+
+        noteBtn.onmouseover = function() {
+          this.innerHTML = "Not<br>Ekle/<br>Sil";
+        }
+  
+        noteBtn.onmouseout = function() {
+          this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg>`;
+        }
+      }
 
       noteBtn.onclick = function() {
         addNotePrompt(cityId, person);
       }
 
-
-      noteBtn.onmouseover = function() {
-        this.innerHTML = "Not<br>Ekle/<br>Sil";
-      }
-
-      noteBtn.onmouseout = function() {
-        this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg>`;
-      }
 
       btnsAbso.appendChild(noteBtn);
     
@@ -807,7 +856,7 @@ function numbersOnCities() {
 
 
 
-  let turkiye = document.querySelectorAll("#turkiye > g"); // استخدم querySelectorAll للحصول على مجموعة من العناصر
+  let turkiye = document.querySelectorAll("#turkiye > g"); 
   turkiye.forEach((sehir) => {
     var rect = sehir.getBoundingClientRect();
     var x = rect.left + window.scrollX + rect.width / 2;
@@ -1164,7 +1213,7 @@ function capitalizeFirstLetters(sentence) {
   const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
   const result = capitalizedWords.join(' ');
 
-  return result;
+  return result.trim();
 }
 
 
